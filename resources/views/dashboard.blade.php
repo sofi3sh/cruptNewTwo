d34aff947a8897e381fec084d21f5a68dad83c63
varchakoleksandr <varchakoleksandr@gmail.com>
2023-06-15 10:50:32 +0300

    style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,39 +9,19 @@
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     <title>Stan's Crypto Terminal</title>
-    <!-- Custom CSS -->
-{{--    <link href="assets/extra-libs/c3/c3.min.css" rel="stylesheet">--}}
-{{--    <link href="assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">--}}
-
-
-
-{{--    <link href="{{asset('assets/extra-libs/c3/c3.min.css')}}" rel="stylesheet">--}}
-
-    <link href="{{ URL::asset('resources/css/style.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ URL::asset('resources/css/c3.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ URL::asset('resources/css/chartist.min.css') }}" rel="stylesheet" type="text/css"/>
-
-{{--    <link href="{{ URL::asset('resources/css/style.css') }}" rel="stylesheet" type="text/css"/>--}}
-
-
-
-
-
-{{--    <link href="resources/css/style.min.css" rel="stylesheet">--}}
-{{--    <link href="resources/css/style.css" rel="stylesheet">--}}
-
-
-{{--    <link href="assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css" rel="stylesheet" />--}}
 
     <!-- Custom CSS -->
-{{--    <link href="dist/css/style.min.css" rel="stylesheet">--}}
+    <link href="{{ asset('assets/extra-libs/c3/c3.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/libs/chartist/dist/chartist.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/extra-libs/jvector/jquery-jvectormap-2.0.5.css') }}" rel="stylesheet">
+    <link href="{{ asset('dist/css/style.min.css') }}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!--[if lt IE 9]-->
+{{--    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>--}}
+{{--    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>--}}
+    <!--[endif]-->
 </head>
 
 <body>
